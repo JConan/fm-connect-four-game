2215a176f52626522092404ea2b6daa90d46ad33
a81615 <johan.chan@externe.bnpparibas.com>
2023-05-12 09:10:00 +0100

    create basic in game styling

--- src/lib/PlayVsPlayer.svelte
+++ src/lib/PlayVsPlayer.svelte
@@ -1,12 +1,102 @@
 <script lang="ts">
     import GameMenu from "./Modals/GameMenu.svelte";
 
-    let menu:GameMenu;
-    $: showMenu = ()=>{ menu && menu.show() }
+    let menu: GameMenu;
+    $: showMenu = () => {
+        menu && menu.show();
+    };
 </script>
 
-<button on:click={showMenu}>
-    menu
-</button>
+<div class="top">
+    <button on:click={showMenu}>MENU</button>
+    <img width="52px" src="/images/logo.svg" alt="logo" />
+    <button>RESTART</button>
+</div>
+
+<div class="container">
+    <div class="player">
+        <p>PLAYER 1</p>
+        <p>12</p>
+        <img src="/images/player-one.svg" alt="player one icon" />
+    </div>
+    <div class="board" aria-label="board">
+        <img
+            src="/images/board-layer-black-large.svg"
+            alt="board layout black"
+        />
+        <img
+            src="/images/board-layer-white-large.svg"
+            alt="board layout white"
+        />
+    </div>
+    <div class="player">
+        <p>PLAYER 2</p>
+        <p>23</p>
+        <img src="/images/player-two.svg" alt="player two icon" />
+    </div>
+</div>
 
 <GameMenu bind:this={menu} />
+
+<style>
+    button {
+        background: #5c2dd5;
+        color: white;
+        height: 39px;
+        width: 86px;
+        border-radius: 20px;
+        border: none;
+    }
+    button:hover {
+        background: #fd6687;
+    }
+    .board {
+        height: 584px;
+        width: 632px;
+        border-radius: 40px;
+    }
+    .board img {
+        position: absolute;
+        width: 632px;
+    }
+    .player {
+        height: 160px;
+        width: 141px;
+        border-radius: 20px;
+        border: 3px solid #000000;
+        box-shadow: 0px 10px 0px 0px #000000;
+        background-color: white;
+        text-align: center;
+        font-size: 20px;
+        font-weight: 700;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+    .player p{
+        margin: 0;
+        line-height: 0;
+    }
+    .player p:nth-child(2) {
+        font-size: 56px;
+    }
+    .player img {
+        position: absolute;
+        top: -30px;
+        left: calc( 50% - 27px );
+    }
+    .container{
+        width: calc( 187px + 60px + 584px + 60px + 187px );
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+    .top{
+        width: 632px;
+        margin: 50px auto;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
--- src/specs/PlayVsPlayer.layout.spec.ts
+++ src/specs/PlayVsPlayer.layout.spec.ts
@@ -2,33 +2,50 @@ import { fireEvent, render, screen } from '@testing-library/svelte'
 import PlayVsPlayer from '../lib/PlayVsPlayer.svelte'
 
 describe('Rules layout', () => {
-    it("should have a button for continue the game", async () => {
+    beforeEach(()=>{
         render(PlayVsPlayer)
+    })
+
+    it("should have a logo", () => {
+        screen.getByAltText(/logo/i)
+    })
+
+    it('should have a restart button', () => {
+        screen.getByText(/^restart$/i)
+    })
+
+    it("should have a board", () => {
+        screen.getByLabelText("board")
+    })
+
+    it("should have players score panel", () => {
+        expect(screen.getAllByAltText(/^player .* icon$/).length).toBe(2)
+    })
+
+    it("should have a button for continue the game", async () => {
         const button = screen.getByText(/continue game/i).parentElement as HTMLAnchorElement
         expect(button).not.toBeVisible()
 
-        const menu = screen.getByText(/menu/)
+        const menu = screen.getByText(/menu/i)
         await fireEvent.click(menu)
         expect(button).toBeVisible()
     })
 
     it("should have a button for restarting the game", async () => {
-        render(PlayVsPlayer)
         const button = screen.getByText(/restart game/i).parentElement as HTMLAnchorElement
         expect(button).not.toBeVisible()
 
-        const menu = screen.getByText(/menu/)
+        const menu = screen.getByText(/menu/i)
         await fireEvent.click(menu)
         expect(button).toBeVisible()
     })
 
     it("should have a button to quit", async () => {
-        render(PlayVsPlayer)
         const button = screen.getByText(/quit/i).parentElement as HTMLAnchorElement
         expect(button.href).toMatch(/\/#$/)
         expect(button).not.toBeVisible()
 
-        const menu = screen.getByText(/menu/)
+        const menu = screen.getByText(/menu/i)
         await fireEvent.click(menu)
         expect(button).toBeVisible()
     })
