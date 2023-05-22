c8149ea91857f2d1bf52e7c79c764c834ec31309
a81615 <johan.chan@externe.bnpparibas.com>
2023-05-22 16:25:55 +0100

    add screen transition effect

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,21 @@
 <script>
   import "./app.css";
   import { state } from "./lib/Stores/Navigation";
+  import { fly } from "svelte/transition";
   import Main from "$lib/Screens/Main.svelte";
   import Rules from "$lib/Screens/Rules.svelte";
+
 
   const screens = {
     menu: Main,
     rules: Rules,
   };
+  const speed= 300;
   $: screen = screens[$state.value];
 </script>
 
-<svelte:component this={screen} />
+{#key screen}
+  <div in:fly={{ y: -500, duration: speed, delay: speed }} out:fly={{ y: -500, duration: speed }} >
+    <svelte:component this={screen} />
+  </div>
+{/key}
