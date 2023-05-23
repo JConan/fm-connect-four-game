f1ee322b98e133dd9ee42a32f7a364c47bf98bb4
a81615 <johan.chan@externe.bnpparibas.com>
2023-05-23 10:50:26 +0100

    add progressive styling for rules screen

--- src/lib/Screens/Rules.svelte
+++ src/lib/Screens/Rules.svelte
@@ -2,34 +2,26 @@
     import Checker from "$lib/Buttons/Checker.svelte";
 </script>
 
-<div class="rules-container">
+<div>
     <h1>Rules</h1>
     <h2>OBJECTIVE</h2>
-    <p>
-        Be the first player to connect 4 of the same colored discs in a row
-        (either vertically, horizontally, or diagonally).
-    </p>
+    <p>Be the first player to connect 4 of the same colored discs in a row (either vertically, horizontally, or diagonally).</p>
     <h2>HOW TO PLAY</h2>
     <ol>
         <li><p>Red goes first in the first game.</p></li>
         <li>
-            <p>
-                Players must alternate turns, and only one disc can be dropped
-                in each turn.
-            </p>
+            <p>Players must alternate turns, and only one disc can be dropped in each turn.</p>
         </li>
-        <li><p>The game ends when there is a 4-in-a-row or a stalemate.</p></li>
+        <li><p>The game ends when there is a 4<wbr />-in-a-row or a stalemate.</p></li>
         <li>
-            <p>
-                The starter of the previous game goes second on the next game.
-            </p>
+            <p>The starter of the previous game goes second on the next game.</p>
         </li>
     </ol>
     <Checker />
 </div>
 
 <style>
-    .rules-container {
+    div {
         background-color: #ffffff;
         box-shadow: 0px 10px 0px 0px #000000;
         height: 537px;
@@ -38,33 +30,57 @@
         top: 0px;
         border-radius: 40px;
         border: 3px solid black;
-        padding-left: 34px;
-        padding-right: 30px;
+        padding: 0 30px;
     }
 
-    .rules-container h1,
-    .rules-container h2 {
+    div h2:nth-child(4) {
+        margin-top: 30px;
+    }
+
+    div h1,
+    div h2 {
         text-transform: uppercase;
     }
-    .rules-container h1 {
+    div h1 {
         font-size: 56px;
         font-weight: 700;
         text-align: center;
     }
-    .rules-container h2 {
+    div h2 {
         font-size: 20px;
         font-weight: 700;
         color: #7945ff;
     }
-    .rules-container ol {
+    div ol {
         font-size: 16px;
         font-weight: 700;
         padding-left: 34px;
+        counter-reset: item;
+        list-style-type: none;
     }
-    .rules-container p {
+
+    div li::before {
+        content: counter(item) " ";
+        counter-increment: item;
+        width: 2em;
+        margin-left: -2em;
+        position: absolute;
+        font-weight: 700;
+    }
+    div p {
         font-size: 16px;
         font-weight: 500;
         mix-blend-mode: normal;
         opacity: 0.66;
+    }
+    @media only screen and (max-width: 768px) {
+        div {
+            height: 586px;
+            width: 335px;
+            padding: 0 22px;
+        }
+        div ol {
+            margin-bottom: 28px;
+        }
     }
 </style>
